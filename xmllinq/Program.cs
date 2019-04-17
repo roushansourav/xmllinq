607ef994acf24d22dc0faecbaba2c3f78b062b4c
Roushan Sourav <roushansourav@outlook.com>
2019-04-17 11:47:48 +0530

    Add project files.

---
+++ xmllinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace xmllinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdocument = XDocument.Load("..\\..\\XMLFile2.xml");
+            xdocument.Save("..\\..\\File1.xml");
+            IEnumerable<XElement> employees = xdocument.Elements();
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+        }
+    }
+}
